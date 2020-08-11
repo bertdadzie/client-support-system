e488066d98f594a8659f90ef760b5e6cf65191bf
unknown <dadzie2017j@gmail.com>
2020-08-11 02:32:03 -0700

    sidebar modified

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -5,14 +5,6 @@
                     <ul id="sidebarnav">
                         <li class="nav-devider"></li>
                         <li class="nav-small-cap">PERSONAL</li>
-                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-gauge"></i><span class="hide-menu">Dashboard <span class="label label-rouded label-themecolor pull-right">4</span></span></a>
-                            <ul aria-expanded="false" class="collapse">
-                                <li><a href="index.php">Minimal </a></li>
-                                <li><a href="index2.php">Analytical</a></li>
-                                <li><a href="index3.php">Demographical</a></li>
-                                <li><a href="index4.php">Modern</a></li>
-                            </ul>
-                        </li>
                         <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-archive"></i><span class="hide-menu">Tickets</span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="ticket.php">Support Ticket</a></li>
